cb63fe07c796e1757026a56f287c69cb5ceed1f3
Camilla Krag Jensen <ckj@reload.dk>
2020-01-24 17:11:37 -0600

    Update bundles to work with core 2

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -6,86 +6,87 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
-    public function registerBundles()
-    {
-        $bundles = [
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AclBundle\AclBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
-            new FOS\RestBundle\FOSRestBundle(),
-            new Sonata\CoreBundle\SonataCoreBundle(),
-            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
-            new Sonata\MediaBundle\SonataMediaBundle(),
-            new Debril\RssAtomBundle\DebrilRssAtomBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle(),
-            new JMS\JobQueueBundle\JMSJobQueueBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+  public function registerBundles()
+  {
+    $bundles = [
+      new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+      new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+      new Symfony\Bundle\TwigBundle\TwigBundle(),
+      new Symfony\Bundle\MonologBundle\MonologBundle(),
+      new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+      new Symfony\Bundle\AclBundle\AclBundle(),
+      new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+      new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+      new FOS\UserBundle\FOSUserBundle(),
+      new FOS\RestBundle\FOSRestBundle(),
+      new Sonata\CoreBundle\SonataCoreBundle(),
+      new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+      new Sonata\MediaBundle\SonataMediaBundle(),
+      new Debril\RssAtomBundle\DebrilRssAtomBundle(),
+      new JMS\SerializerBundle\JMSSerializerBundle(),
+      new JMS\JobQueueBundle\JMSJobQueueBundle(),
+      new JMS\DiExtraBundle\JMSDiExtraBundle(),
+      new JMS\AopBundle\JMSAopBundle(),
+      new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+      new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+      new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
 
-            new Os2Display\MediaBundle\Os2DisplayMediaBundle(),
-            new Os2Display\CoreBundle\Os2DisplayCoreBundle(),
-            new Os2Display\AdminBundle\Os2DisplayAdminBundle(),
-            new Os2Display\DefaultTemplateBundle\Os2DisplayDefaultTemplateBundle(),
+      new Os2Display\MediaBundle\Os2DisplayMediaBundle(),
+      new Os2Display\CoreBundle\Os2DisplayCoreBundle(),
+      new Os2Display\AdminBundle\Os2DisplayAdminBundle(),
+      new Os2Display\DefaultTemplateBundle\Os2DisplayDefaultTemplateBundle(),
+      new Os2Display\CampaignBundle\Os2DisplayCampaignBundle(),
+      new Os2Display\ScreenBundle\Os2DisplayScreenBundle(),
 
-//            new Itk\ExchangeBundle\ItkExchangeBundle(),
-//            new Itk\HorizonTemplateBundle\ItkHorizonTemplateBundle(),
-//            new Itk\VimeoBundle\ItkVimeoBundle(),
+      new Os2Display\HorizonTemplateBundle\Os2DisplayHorizonTemplateBundle(),
+      new Os2Display\ExchangeBundle\Os2DisplayExchangeBundle(),
+      new Os2Display\VimeoBundle\Os2DisplayVimeoBundle()
+    ];
 
-        ];
+    if (in_array($this->getEnvironment(), ['dev', 'test', 'acceptance'], true)) {
+      $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+      $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+      $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
 
-        if (in_array($this->getEnvironment(), ['dev', 'test', 'acceptance'], true)) {
-            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-
-            if ('dev' === $this->getEnvironment()) {
-                $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-                $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
-            }
-        }
-
-        return $bundles;
+      if ('dev' === $this->getEnvironment()) {
+        $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
+      }
     }
 
-    public function getRootDir()
-    {
-        return __DIR__;
-    }
+    return $bundles;
+  }
 
-    public function getCacheDir()
-    {
-        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
-    }
+  public function getRootDir()
+  {
+    return __DIR__;
+  }
 
-    public function getLogDir()
-    {
-        return dirname(__DIR__).'/var/logs';
-    }
+  public function getCacheDir()
+  {
+    return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+  }
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load(function (ContainerBuilder $container) {
-            $container->setParameter('container.autowiring.strict_mode', true);
-            $container->setParameter('container.dumper.inline_class_loader', true);
+  public function getLogDir()
+  {
+    return dirname(__DIR__).'/var/logs';
+  }
 
-            $container->addObjectResource($this);
-        });
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
-    }
+  public function registerContainerConfiguration(LoaderInterface $loader)
+  {
+    $loader->load(function (ContainerBuilder $container) {
+      $container->setParameter('container.autowiring.strict_mode', true);
+      $container->setParameter('container.dumper.inline_class_loader', true);
 
-    public function __construct($environment, $debug)
-    {
-        // Force the timezone to be UTC.
-        date_default_timezone_set('UTC');
-        parent::__construct($environment, $debug);
-    }
+      $container->addObjectResource($this);
+    });
+    $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+  }
+
+  public function __construct($environment, $debug)
+  {
+    // Force the timezone to be UTC.
+    date_default_timezone_set('UTC');
+    parent::__construct($environment, $debug);
+  }
 }
